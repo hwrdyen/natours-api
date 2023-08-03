d6523a1c0a0ea294d8b9604ebe22022cee0fb57b
Howard Yen <hwrdpvt@gmail.com>
2023-08-03 20:38:09 +0800

    Removed mongoDB updated warning

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
 //   .catch((err) => console.log('ERROR'));
